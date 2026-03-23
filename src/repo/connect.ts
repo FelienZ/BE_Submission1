297d30d0fc699cf7538ca4281d9b193340fd23ec
FelienZ <rajabfadh@gmail.com>
2026-03-23 23:12:17 +0700

    refactor(repo): change connection config with PG prefix and use Pool connection

--- src/repo/connect.ts
+++ src/repo/connect.ts
@@ -1,11 +1,12 @@
-import { configDotenv } from 'dotenv'
-import pg, { Client,Connection } from 'pg' //ex. unused import w rule unused locals
+import { configDotenv } from 'dotenv';
+import pg, { Pool } from 'pg'; 
 
-configDotenv()
-//pake pool/dbconn buat maxConn, waitConn
-export const DBClient:pg.Connection = new Connection({
-    host:process.env.APP_HOST||'localhost',
-    port: Number(process.env.DB_PORT) || 5432,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-})
+configDotenv();
+//pake pool/dbconn buat maxConn, waitConn -> env pake PG biar auto readable oleh migration
+export const DBClient:pg.Pool = new Pool({
+  host:process.env.PGHOST||'localhost',
+  port: Number(process.env.PGPORT) || 5432,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE
+});
