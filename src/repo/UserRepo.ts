90c821ea11cf9d0555ab0187ec3c414f4dddc4fd
Kuchihakch <rajabfadhlyhk@gmail.com>
2026-03-25 20:26:36 +0700

    refactor(repo): Change error handling in repository layer & impelement not found error

--- src/repo/UserRepo.ts
+++ src/repo/UserRepo.ts
@@ -2,6 +2,7 @@ import type { Pool } from 'pg';
 import type { UserRepository } from '../models/interfaces/repository.js';
 import type { User } from '../models/types/user.js';
 import UserMapper from './utils/UserMapper.js';
+import { NotFoundError } from '../services/error/NotFoundError.js';
 
 export class UserRepo implements UserRepository {
   dbClient: Pool;
@@ -14,50 +15,42 @@ export class UserRepo implements UserRepository {
   async createUser(payload: User): Promise<string> {
     const query = 'INSERT INTO users (id, user_name, email, password, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id';
     const values = [payload.id, payload.name, payload.email, payload.password, payload.createdAt, payload.updatedAt];
-    try {
-      const result =await this.dbClient.query(query, values);
-      const newUserId = result.rows[0].id;
-      return newUserId;
-    } catch (error) {
-      throw new Error('Failed to create user', {cause: error});
-    }
+    const result =await this.dbClient.query(query, values);
+    const newUserId = result.rows[0].id;
+    return newUserId;
   }
   async getUserById(userId: string): Promise<User | null> {
     const query = 'SELECT * FROM users WHERE id = $1';
     const values = [userId];
-    try {
-      const result = await this.dbClient.query(query, values);
-      return UserMapper(result.rows[0])|| null;
-    } catch (error) {
-      throw new Error('Failed to get user by ID', {cause: error});
+    const result = await this.dbClient.query(query, values);
+    if (result.rows.length == 0){
+      return null;
     }
+    return UserMapper(result.rows[0]);
   }
   async getUserByEmail(email: string): Promise<User | null> {
     const query = 'SELECT * FROM users WHERE email = $1';
     const values = [email];
-    try {
-      const result = await this.dbClient.query(query, values);
-      return UserMapper(result.rows[0]) || null;
-    } catch (error) {
-      throw new Error('Failed to get user by email', {cause: error});
+    const result = await this.dbClient.query(query, values);
+    if (result.rows.length == 0){
+      return null;
     }
+    return UserMapper(result.rows[0]);
   }
   async updateUser( payload: User): Promise<void> {
     const query = 'UPDATE users SET user_name = $1, email = $2, password = $3, updated_at = $4 WHERE id = $5';
     const values = [payload.name, payload.email, payload.password, payload.updatedAt, payload.id];
-    try {
-      await this.dbClient.query(query, values);
-    } catch (error) {
-      throw new Error('Failed to update user', {cause: error});
+    const result = await this.dbClient.query(query, values);
+    if (result.rowCount == 0){
+      throw new NotFoundError('User Tidak Ditemukan');
     }
   }
   async deleteUser(userId: string): Promise<void> {
     const query = 'DELETE FROM users WHERE id = $1';
     const values = [userId];
-    try {
-      await this.dbClient.query(query, values);
-    } catch (error) {
-      throw new Error('Failed to delete user', {cause: error});
+    const result = await this.dbClient.query(query, values);
+    if (result.rowCount == 0){
+      throw new NotFoundError('User Tidak Ditemukan');
     }
   }
 }
