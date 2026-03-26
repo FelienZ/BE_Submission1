a6888523fd21a40e22ac5fbbd31f9093379a4945
Kuchihakch <rajabfadhlyhk@gmail.com>
2026-03-26 10:09:43 +0700

    chore(config): restructure configuration for jest

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,12 +1,13 @@
-import { createDefaultPreset } from "ts-jest";
+import { createDefaultPreset } from 'ts-jest';
 
 const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 export default {
-  testEnvironment: "node",
+  testEnvironment: 'node',
   transform: {
     ...tsJestTransformCfg,
+    '^.+\\.tsx?$': ['ts-jest', { useESM: true }], // ESM
   },
   preset: 'ts-jest/presets/default-esm',
   moduleNameMapper: {
@@ -14,7 +15,4 @@ export default {
   },
   testMatch: ['**/tests/**/*.test.ts'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-  transform: {
-    '^.+\\.tsx?$': ['ts-jest', { useESM: true }], // ESM
-  },
 };
